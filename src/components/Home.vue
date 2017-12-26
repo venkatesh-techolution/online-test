eb014f5ca160d195bb1cb33805210275f86384bf
Naren <narenderv7@gmail.com>
2017-12-27 01:09:59 +0530

    Added Developer Test Page

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -3,19 +3,9 @@
     <h1>{{ msg }}</h1>
     <h2>Essential Links</h2>
     <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-      <br>
-      <li><a href="http://vuejs-templates.github.io/webpack/" target="_blank">Docs for This Template</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
+      <li v-for="(link, i) in links" :key="i">
+        <a :href="`test/${link.test}`" @click="selectedTest = link.test">{{ link.test }}</a>
+      </li>
     </ul>
   </div>
 </template>
@@ -25,7 +15,18 @@ export default {
   name: 'HelloWorld',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      msg: 'Welcome to Techolution Online Test',
+      selectedTest: undefined,
+      links: [
+        {
+          test: 'UI Developer',
+          description: 'UI Developer'
+        },
+        {
+          test: 'Java Developer',
+          description: 'Java Developer'
+        }
+      ]
     }
   }
 }
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,23 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from '@/components/Home'
+import Test from '@/components/Test'
 
 Vue.use(Router)
 
 export default new Router({
+  mode: 'history',
+  base: '/',
   routes: [
     {
       path: '/',
       name: 'Home',
       component: Home
+    },
+    {
+      path: '/test/:testId',
+      name: 'Test Page',
+      component: Test
     }
   ]
 })
